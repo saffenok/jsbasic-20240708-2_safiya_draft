bd1bad76e406a207c5c68c39d90610a316cf3d7a
Safiya <gss.777@mail.ru>
2024-07-29 01:35:31 +0300

    задача 3 из модуля 2 и задачи 1 и 2 из модуля 3

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,10 @@
 function filterRange(arr, a, b) {
   // ваш код...
+  let newArray = [];
+  let arrayElem = arr.filter((number) => {
+    if ((number >= a && number <= b) || (number <= a && number >= b)) {
+      newArray.push(number);
+    }
+  });
+  return newArray;
 }
