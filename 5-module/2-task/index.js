4322b35dc41c0b55a5397a7b2d6c3e6eec559b16
Safiya <gss.777@mail.ru>
2024-08-28 00:59:47 +0300

    задача 5-2

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,14 @@
 function toggleText() {
   // ваш код...
+  let btn = document.querySelector('.toggle-text-button');
+  let text = document.querySelector('#text');
+  let i = 0;
+  btn.addEventListener('click', () => {
+    if (i == 0) {
+      text.hidden = true;
+    } else {
+      text.hidden = false;
+    }
+    i++;
+  });
 }
