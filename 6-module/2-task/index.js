f03e25d61cad6267961095098307df91666077ca
Safiya Mac <safok765@gmail.com>
2026-01-26 14:29:38 +0300

    задачи 6-2 и 6-3

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,57 @@
+import createElement from '../../assets/lib/create-element.js';
+
+// {
+//   name: "Laab kai chicken salad",
+//   price: 10,
+//   category: "salads",
+//   image: "laab_kai_chicken_salad.png",
+//   id: "laab-kai-chicken-salad"
+// }
+
 export default class ProductCard {
   constructor(product) {
+    this.product = product;
+    this.container = createElement(`
+      <div class="card">
+        <div class="card__top">
+          <img class="card__image" alt="product">
+          <span class="card__price">€</span>
+        </div>
+        <div class="card__body">
+          <div class="card__title">NAME</div>
+          <button type="button" class="card__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+        </div>
+      </div>
+    `);
+
+    this.fillCard();
+    this.addProduct();
+  }
+
+  fillCard() {
+    this.container.querySelector('img.card__image').src = `/assets/images/products/${this.product.image}`;
+    this.container.querySelector('span.card__price').innerHTML = `€${this.product.price.toFixed(2)}`;
+    this.container.querySelector('div.card__title').innerHTML = `${this.product.name}`;
+  }
+
+  addProduct() {
+    this.container.addEventListener('product-add', event => {
+      console.log(event.detail, 'добавлен в корзину');
+    });
+
+    this.container.addEventListener('click', event => {
+      if (!event.target.closest('button.card__button')) return;
+
+      this.container.dispatchEvent(new CustomEvent('product-add', {
+        detail: this.product.id,
+        bubbles: true,
+      }));
+    });
+  }
+
+  get elem() {
+    return this.container;
   }
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,7 +1,122 @@
 import createElement from '../../assets/lib/create-element.js';
 
+// [
+//   {
+//     name: 'Penang shrimp',
+//     price: 16,
+//     image: 'penang_shrimp.png',
+//     id: 'penang-shrimp'
+//   },
+//   {} ...
+
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+    this.wrapper = null;
+    this.clickCounter = 0;
+
+    this.render();
+
+    let carouselArrowLeft = this.wrapper.querySelector('.carousel__arrow.carousel__arrow_left');
+    // скрыть стрелку для первого слайда
+    if (this.clickCounter == 0) {
+      carouselArrowLeft.style.display = 'none';
+    }
+
+    this.initEventListeners();
+  }
+
+  render() {
+    this.wrapper = createElement(`
+      <div class="carousel">
+        <!--Кнопки переключения-->
+        <div class="carousel__arrow carousel__arrow_right">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </div>
+        <div class="carousel__arrow carousel__arrow_left">
+          <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+        </div>
+        <div class="carousel__inner">
+          ${this.slides.map(slide => this.renderSlide(slide)).join('')}
+        </div>
+      </div>
+    `);
+  }
+
+  renderSlide(slide) {
+    return `
+      <div class="carousel__slide" data-id="${slide.id}">
+          <img src="/assets/images/carousel/${slide.image}" class="carousel__img" alt="slide">
+          <div class="carousel__caption">
+            <span class="carousel__price">€${slide.price.toFixed(2)}</span>
+            <div class="carousel__title">${slide.name}</div>
+            <button type="button" class="carousel__button">
+              <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+            </button>
+          </div>
+      </div>
+    `
+  }
+
+  initEventListeners() {
+    this.wrapper.addEventListener('click', event => {
+      if (event.target.closest('.carousel__arrow'))
+        this.arrowClick(event);
+      if (event.target.closest('.carousel__button'))
+        this.buttonAddClick(event);
+    });
+
+    this.wrapper.addEventListener('product-add', event => {
+      console.log(event.detail, 'добавлен в корзину');
+    });
+  }
+
+  arrowClick(event) {
+    let carouselInner = this.wrapper.querySelector('.carousel__inner');
+    let carouselWidth = carouselInner.offsetWidth;
+
+    // для правой стрелки
+    if (event.target.closest('.carousel__arrow.carousel__arrow_right')) {
+      this.clickCounter++;
+      carouselInner.style.transform = `translateX(${-carouselWidth * this.clickCounter + "px"})`;
+    }
+
+    // для левой стрелки
+    if (event.target.closest('.carousel__arrow.carousel__arrow_left')) {
+      this.clickCounter--;
+      carouselInner.style.transform = `translateX(${-carouselWidth * this.clickCounter + "px"})`;
+    }
+
+    // настройка видимости стрелок
+    this.arrowVisible();
+  }
+
+  arrowVisible() {
+    let carouselArrowLeft = this.wrapper.querySelector('.carousel__arrow.carousel__arrow_left');
+    let carouselArrowRight = this.wrapper.querySelector('.carousel__arrow.carousel__arrow_right');
+    let carouselSlides = this.wrapper.querySelectorAll('.carousel__slide');
+
+    // скрыть стрелки для крайних слайдов
+    if (this.clickCounter == 0) {
+      carouselArrowLeft.style.display = 'none';
+    } else if (this.clickCounter == (carouselSlides.length - 1)) {
+      carouselArrowRight.style.display = 'none';
+    } else {
+      carouselArrowLeft.style.display = '';
+      carouselArrowRight.style.display = '';
+    }
+  }
+
+  buttonAddClick(event) {
+    let slide = event.target.closest('div.carousel__slide');
+
+    this.wrapper.dispatchEvent(new CustomEvent('product-add', {
+      detail: slide.dataset.id,
+      bubbles: true,
+    }));
+  }
+
+  get elem() {
+    return this.wrapper;
   }
 }
